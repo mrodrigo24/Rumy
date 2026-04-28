b2c7dd13385ac49ea2bb37e2b26e816a5890486d
miguel <mrodrigo577@alumnes.politecnicllevant.cat>
2026-04-28 14:34:36 -0700

    creamos los jugadores

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,8 +1,26 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Jugador {
     private String nombre;
     private int puntos;
+    private List<Carta> mano = new ArrayList<>();
+
 
     public Jugador(String nombre){
         this.nombre=nombre;
     }
+    public void recibirCarta(Carta c){
+        mano.add(c);
+    }
+
+    public void imprimir(){
+        Iterator<Carta> it = mano.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+
+
 }
