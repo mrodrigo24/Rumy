40c4406656fded5ae143b0267a2c1068fdb6823b
miguel <mrodrigo577@alumnes.politecnicllevant.cat>
2026-04-30 20:24:28 -0700

    clase Tablero

--- src/Juego.java
+++ src/Juego.java
@@ -1,10 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Juego {
     public static void main(String[] args) {
-        Mazo maz=new Mazo();
-        Carta carta1=maz.cogerCarta();
-        Carta carta2=maz.cogerCarta();
-        System.out.println(carta1);
-        System.out.println(carta2);
-        System.out.println(maz.cuantasCartasQuedan());
+        Mazo maz = new Mazo();
+        List<Carta> descarte = new ArrayList<>();
+        List<Jugador> jugadores = new ArrayList<>();
+        int turno=0;
+            for (int i = 0; i < 4; i++) {
+                jugadores.add(new Jugador("Jugador" + (i)));
+            }
+            turno = (turno + 1) % 4;
+
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 10; j++) {
+                    jugadores.get(i).recibirCartas(maz.cogerCarta());
+                }
+            }
+            while (turno<4){
+                descarte.add(maz.cogerCarta());
+                System.out.println(turno);
+                jugadores.get(turno).mostrarMano();
+                jugadores.get(turno).recogerDescarte(descarte);
+
+                turno = (turno+1)%4;
+            }
+
+
     }
 }
