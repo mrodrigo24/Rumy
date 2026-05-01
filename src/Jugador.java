e8cdeebdfbc964b76b4f1a3bbdfb62f3e6c88c35
miguel <mrodrigo577@alumnes.politecnicllevant.cat>
2026-05-01 09:37:18 -0700

    numeramos las cartas por jugador

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,10 +1,68 @@
-public class Jugador {
-    private String nombre;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
 
-    public Jugador(String nombre){
-        this.nombre=nombre;
+public class Jugador {
+    private String numeroDeJugador;
+    private int puntos;
+    List<Carta> cartasPorJugador;
+    private boolean haSalido=false;
+
+    public Jugador (String numeroDeJugador){
+     this.numeroDeJugador=numeroDeJugador;
+     this.puntos=0;
+     this.cartasPorJugador=new ArrayList<Carta>();
+}
+    public void recibirCartas(Carta carta){
+            cartasPorJugador.add(carta);
     }
 
+    public void mostrarMano() {
+        System.out.println("Mano de " + numeroDeJugador + ":");
+        Iterator<Carta> it = cartasPorJugador.iterator();
+        while (it.hasNext()) {
+            Carta c = it.next();
+            System.out.println("- " + c);
+        }
+        System.out.println();
+    }
+
+    public void recogerDescarte(List<Carta> listacomun){
+            if(!listacomun.isEmpty()){
+                Carta c=listacomun.removeFirst();
+                this.cartasPorJugador.add(c);
+                System.out.println(c);
+            }
+    }
+
+    public void setHasalido(boolean haSalido){
+        this.haSalido=haSalido;
+    }
+
+    public void elegirCartas(Carta cartaElegida){
+        Iterator<Carta> it = cartasPorJugador.iterator();
+        List <Carta>manoDelJugador=new ArrayList<>();
+        while(it.hasNext()){
+            Carta c=it.next();
+                manoDelJugador.add(c);
+                it.remove();
+
+        }
+
+    }
+
+
+   /* public void tirarCartas(List <Carta> listaJugador){
+        System.out.println("Que cartas quieres tirar");
+        Scanner sc = new Scanner(System.in);
+
+        if(!listaJugador.isEmpty()){
+
+            this.cartasPorJugador.remove(c);
+            System.out.println(c);
+        }
+    }*/
 
 
 }
