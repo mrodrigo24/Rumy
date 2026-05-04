828e873b3cb902f9551dc1eb08ffecb2a8329fa5
miguel <mrodrigo577@alumnes.politecnicllevant.cat>
2026-05-04 09:55:48 +0200

    seguimos con el metodo Grupos

--- src/ValidadorRummy.java
+++ src/ValidadorRummy.java
@@ -24,7 +24,7 @@ public class ValidadorRummy {
         }
 
         public int sumaPuntos(List<Carta> listaRecibida){
-            //Collections.sort(listaRecibida);
+            Collections.sort(listaRecibida);
             Iterator<Carta> it = listaRecibida.iterator();
             while(it.hasNext()){
                 Carta actual= it.next();
@@ -33,6 +33,36 @@ public class ValidadorRummy {
             //System.out.println("El valor es "+sumaPuntos);
             return sumaPuntos;
         }
+
+        public boolean Grupos (List<Carta> listaRecibida){
+            Iterator<Carta> it = listaRecibida.iterator();
+            List<Palos> palosYaVistos=new ArrayList<>();
+            Palos ultimoPalo=null;
+            int contador=0;
+            if(listaRecibida.size()<3){
+                return  false;
+            }
+            int numeroReferencia=listaRecibida.get(0).getValor().getNumero();
+            while(it.hasNext()){
+                Carta actual=it.next();
+                if(palosYaVistos.contains(actual.getPalo())){
+                    return false;
+                } else {
+                    palosYaVistos.add(actual.getPalo());
+                }
+
+                if(actual.getValor().getNumero()!=numeroReferencia){
+                    return false;
+                }
+                Palos paloActual=actual.getPalo();
+                if (!paloActual.equals(ultimoPalo)){
+                    contador++;
+                }
+                ultimoPalo=paloActual;
+            }
+            return true;
+        }
+
     }
 
 
