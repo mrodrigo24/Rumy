3c7651d97d73bcde06bb4f43fe7bf3a276bca2d9
miguel <mrodrigo577@alumnes.politecnicllevant.cat>
2026-06-02 12:59:55 +0200

    implementadno todo y cazando errores

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Instanciamos el objeto del juego (Se ejecuta el constructor)
+
         Juego partida = new Juego();
 
         // Arrancamos la partida
